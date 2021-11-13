d128b558e05a9cb59c5b9d958e94db5511d527e9
Aaron Sarazan <aaron@sarazan.net>
2021-11-13 15:12:22 -0800

    remove unneeded field

--- types/users/UserContact.ts
+++ types/users/UserContact.ts
@@ -1,6 +1,5 @@
 export interface UserContact {
   id: string; ///Make a version of phone number
   phoneNumber: string;
-  gamefaceUserId?: string; /// If nil, UserContact has not signed up
   test: number;
 }
