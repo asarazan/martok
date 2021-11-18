f1703cbf84e890b71e8438f78296c84c3b8ba2c8
Aaron Sarazan <aaron@sarazan.net>
2021-11-18 15:59:16 -0800

    add deduping of types

--- types/Polymorphism.ts
+++ types/Polymorphism.ts
@@ -1,12 +1,34 @@
-export type Poly = {
-  foo: string;
-} & (
+export type SimplePoly =
   | {
-      // type: "something";
+      foo: string;
       bar: number;
     }
   | {
-      // type: "else";
+      foo: string;
       baz: boolean;
-    }
-);
+    };
+
+// export type Poly = {
+//   foo: string;
+// } & (
+//   | {
+//       somethingOrElse: "something";
+//       numberOrBoolean: number;
+//       reference: Reference1;
+//       stringOrReference: string;
+//     }
+//   | {
+//       somethingOrElse: "else";
+//       numberOrBoolean: boolean;
+//       reference: Reference2;
+//       stringOrReference: Reference1;
+//     }
+// );
+//
+// export type Reference1 = {
+//   stringOrNumber: string;
+// };
+//
+// export type Reference2 = {
+//   stringOrNumber: number;
+// };
