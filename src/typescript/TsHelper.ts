d9d0b51986da370e89f047cae995e1bb8ae0de3f
Aaron Sarazan <aaron@sarazan.net>
2021-11-22 22:00:18 -0800

    slightly more test coverage

--- src/typescript/TsHelper.ts
+++ src/typescript/TsHelper.ts
@@ -1,12 +1,7 @@
-import { Program } from "typescript";
 import _ from "lodash";
 import * as path from "path";
 
 export class TsHelper {
-  private readonly checker = this.program.getTypeChecker();
-
-  public constructor(private readonly program: Program) {}
-
   public static getBaseFileName(file: string): string {
     return _(file.split(path.sep))
       .last()!
