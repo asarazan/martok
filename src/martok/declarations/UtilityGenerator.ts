b576541e73972a02257588f64a92b18feb0ef093
Aaron Sarazan <aaron@sarazan.net>
2022-05-07 11:49:08 -0700

    full support for critical utility types

--- src/martok/declarations/UtilityGenerator.ts
+++ src/martok/declarations/UtilityGenerator.ts
@@ -1,8 +1,10 @@
 import {
+  Collection,
   isLiteralTypeNode,
   isStringLiteral,
   isStringLiteralLike,
   isTypeReferenceNode,
+  isUnionTypeNode,
   LiteralTypeNode,
   StringLiteral,
   StringLiteralType,
@@ -13,33 +15,80 @@ import { kotlin } from "../../kotlin/Klass";
 import { Martok } from "../Martok";
 import Klass = kotlin.Klass;
 
+const supportedTypes = ["Omit", "Pick", "Required", "Partial"];
+
 export class UtilityGenerator {
   public constructor(private readonly martok: Martok) {}
 
   public generate(name: string, type: TypeNode): kotlin.Klass | undefined {
     if (!isTypeReferenceNode(type)) return undefined;
-    const typeName = type.typeName.getText();
-    if (typeName === "Omit") return this.generateOmit(name, type);
-    return undefined;
-  }
 
-  private generateOmit(name: string, type: TypeReferenceNode): Klass {
-    const klasses = this.martok.declarations.klasses;
-    const [base, ...args] = type.typeArguments!;
-    const omitNames = args
-      .filter((value) => isLiteralTypeNode(value))
-      .map((value) => (value as LiteralTypeNode).literal)
-      .map((value) => (value as StringLiteral).text);
-    const omitSet = new Set(omitNames);
-    const result = klasses.generate(base, {
+    const typeName = type.typeName.getText();
+    if (!supportedTypes.includes(typeName)) return undefined;
+
+    const [base, args] = type.typeArguments!;
+    const memberNames = this.getNameUnion(args);
+
+    const result = this.martok.declarations.klasses.generate(base, {
       forceName: name,
     }) as Klass;
-    result.setCtorArgs(
-      ...result.ctor.filter((value) => !omitSet.has(value.name))
-    );
-    result.setMembers(
-      ...result.members.filter((value) => !omitSet.has(value.name))
-    );
+
+    if (typeName === "Omit") {
+      result.setCtorArgs(
+        ...result.ctor.filter((value) => !memberNames.has(value.name))
+      );
+      result.setMembers(
+        ...result.members.filter((value) => !memberNames.has(value.name))
+      );
+    }
+
+    if (typeName === "Pick") {
+      result.setCtorArgs(
+        ...result.ctor.filter((value) => memberNames.has(value.name))
+      );
+      result.setMembers(
+        ...result.members.filter((value) => memberNames.has(value.name))
+      );
+    }
+
+    if (typeName === "Partial") {
+      for (const value of result.ctor) {
+        value.nullable = true;
+        value.value = "null";
+      }
+      for (const value of result.members) {
+        value.nullable = true;
+        value.value = "null";
+      }
+    }
+
+    if (typeName === "Required") {
+      for (const value of result.ctor) {
+        value.nullable = false;
+        delete value.value;
+      }
+      for (const value of result.members) {
+        value.nullable = false;
+        delete value.value;
+      }
+    }
     return result;
+  }
+
+  private getNameUnion(type: TypeNode | undefined): Set<string> {
+    if (type) {
+      if (isLiteralTypeNode(type)) {
+        return new Set([(type.literal as StringLiteral).text]);
+      }
+      if (isUnionTypeNode(type)) {
+        return new Set(
+          type.types.map(
+            (value) =>
+              ((value as LiteralTypeNode).literal as StringLiteral).text
+          )
+        );
+      }
+    }
+    return new Set();
   }
 }
--- tests/comparisons/single/utilityTypes.d.ts
+++ tests/comparisons/single/utilityTypes.d.ts
@@ -1,7 +1,7 @@
 export type WithOmit = Omit<Base, "bar">;
 export type WithPick = Pick<Base, "bar" | "baz">;
 export type WithPartial = Partial<Base>;
-// export type WithRequired = Required<Base>;
+export type WithRequired = Required<Base>;
 
 export type Base = {
   foo: string;
