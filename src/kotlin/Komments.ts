ec9e9c1a64b5d53f0f29711f232d962e54394bbd
Aaron Sarazan <aaron@sarazan.net>
2022-11-08 20:59:48 -0800

    revert to older node version compat

--- src/kotlin/Komments.ts
+++ src/kotlin/Komments.ts
@@ -1,6 +1,8 @@
+import _ from "lodash";
+
 export function convertCommentText(comment: string): string {
   const linkRe = /\{\s*@link (\w*)\s*\}/g;
-  comment = comment.replaceAll(linkRe, (substring: string, args: string) => {
+  comment = _.replace(comment, linkRe, (substring: string, args: string) => {
     return `[${args}]`;
   });
   return comment;
