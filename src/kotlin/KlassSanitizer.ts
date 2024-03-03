4dfb2eab65194077cbe5f7047afbcd43d17c8117
Aaron Sarazan <aaron@sarazan.net>
2024-03-02 16:05:36 -0800

    some niceties for newline handling that makes things prettier

---
+++ src/kotlin/KlassSanitizer.ts
@@ -0,0 +1,7 @@
+export function sanitizeKlassOutput(string: string[]): string[] {
+  return string;
+}
+
+export function sanitizeKlassString(string: string): string {
+  return string.replace("\n\n\n", "\n\n");
+}
